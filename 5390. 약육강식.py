41d7c3e1872b13e4a3eb4c28565c767211af9102
p-miina <116613061+p-miina@users.noreply.github.com>
2023-03-09 17:26:56 +0900

    Update 5390. 약육강식.py

--- 5390. 약육강식.py
+++ 5390. 약육강식.py
@@ -2,11 +2,12 @@ n = int(input())
 
 sharks = list(map(int, input().split()))
 
+#dp[i] 값 1로 초기화(시작 상어 +1)
 dp = [1 for i in range(n)]
 
 for i in range(n):
     for j in range(i):
-        if sharks[i] > sharks[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+        if sharks[i] > sharks[j]:           # 현재 상어와 앞에 있는 상어의 크기 비교
+            dp[i] = max(dp[i], dp[j]+1) # 현재 상어가 더 크면 앞의 상어 중 dp의 최대값에 1 더해줌 
 
 print(max(dp))
